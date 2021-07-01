7963d8442a1b377fe866c97bd4ff013952964fe8
charliechauri <me@charliechauri.com>
2021-06-30 22:01:58 -0500

    chore: add graphl-kotlin dependency
    - Add dependency
    - Setup package to include in the GrapQL service

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("com.expediagroup:graphql-kotlin-spring-server:5.0.0-alpha.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
