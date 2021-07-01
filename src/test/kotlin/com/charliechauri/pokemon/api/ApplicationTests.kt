5713c79a774d43d3dd8a6e7fc2264038c58ad980
charliechauri <me@charliechauri.com>
2021-06-30 19:16:07 -0500

    feat: initial commit
    Setup repository using Spring initializr
    
    More details: https://start.spring.io

---
+++ src/test/kotlin/com/charliechauri/pokemon/api/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.charliechauri.pokemon.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
